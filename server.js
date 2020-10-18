b4ac031c30b99fc71a3352b165383b530d77a0f6
Henrique Silva <henrique9811@hotmail.com>
2020-10-18 14:46:27 -0300

    controle de usuário feito

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const io = require('socket.io')(server);
 
 
 let mensagens = [];
+let usuariosConectados = [];
 
 io.on("connection",socket => {
 
@@ -15,6 +16,28 @@ io.on("connection",socket => {
         console.log(mensagens);
         socket.broadcast.emit('mensagemRecevida',envio);
     })
+
+    socket.on('usuarioConectado',nome => {
+        if(nome == null)
+        console.log("usuario não logado conectado");
+        else{
+        console.log("usuario conectado");
+        let novoUsuario = {
+            nome:nome,
+            socketId:socket.id
+        }
+        let index = usuariosConectados.findIndex((usuario => usuario.nome == nome));
+
+        if(index == -1)
+            usuariosConectados.push(novoUsuario);
+        else
+            usuariosConectados[index].socketId = socket.id;
+
+        console.log(usuariosConectados);
+    }
+    })
+
+
 });
 
 server.listen(3002);
