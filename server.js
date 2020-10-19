4c2433e13b2f6811f5ee3bf0f730f7ca524099e7
Henrique Silva <henrique9811@hotmail.com>
2020-10-19 16:07:06 -0300

    conexão com banco e salas feitas

--- server.js
+++ server.js
@@ -2,26 +2,63 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const axios = require('axios');
 
 
-let mensagens = [];
 let usuariosConectados = [];
 
-io.on("connection",socket => {
 
-    socket.emit('ultimasMensagens',mensagens);
+
+
+io.on("connection",socket => {
+    let mensagens = [];
+    let sala;
+
+    objectToString = (obj) => {
+        str = obj.map(obj => {
+            return obj.usuario + ":" + obj.mensagem;
+        })
+        return str;
+    }
 
     socket.on('sendMessage', envio =>{
         mensagens.push(envio);
-        console.log(mensagens);
-        socket.broadcast.emit('mensagemRecevida',envio);
+        let updateChat ={ 
+            mensagens : objectToString(mensagens).toString()
+        };
+        axios.post('http://localhost:3001/updateChat/'+sala,updateChat).then(res =>{
+            socket.to(sala).broadcast.emit('mensagemRecevida',envio);
+        }).catch(error => {
+            console.log(error.data);
+        });
     })
+
+    socket.on('entrarSala',nomeDaSala =>{
+        sala = nomeDaSala;
+        console.log('usuario entrando na sala:'+nomeDaSala);
+        socket.join(nomeDaSala);
+        axios.get('http://localhost:3001/chat/'+sala).then(res =>{
+            let ultimasMensagens = res.data;
+            ultimasMensagens = ultimasMensagens.split(",");
+            mensagens = ultimasMensagens.map(mensagem => {
+                mensagem = mensagem.split(":");
+                return {
+                    usuario: mensagem[0],
+                    mensagem : mensagem[1]
+                }
+            })
+            console.log(mensagens);
+            socket.emit('mensagensAntigas',mensagens);
+        }).catch(error => {
+            console.log(error);
+        })
+    })
+
 
     socket.on('usuarioConectado',nome => {
         if(nome == null)
-        console.log("usuario não logado conectado");
+        return 0;
         else{
-        console.log("usuario conectado");
         let novoUsuario = {
             nome:nome,
             socketId:socket.id
@@ -33,7 +70,6 @@ io.on("connection",socket => {
         else
             usuariosConectados[index].socketId = socket.id;
 
-        console.log(usuariosConectados);
     }
     })
 
